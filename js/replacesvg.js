df4d72d4f70f5194d1d5a1966bb0e574f054ea9a
Johannes Zierenberg <you@example.com>
2018-08-31 11:04:31 +0200

    fixed php stuff

--- js/replacesvg.js
+++ js/replacesvg.js
@@ -1,39 +1,53 @@
 /*
  * Replace all SVG images with inline SVG
  */
-document.querySelectorAll('img.svg').forEach(function(img){
-    var imgID = img.id;
-    var imgClass = img.className;
-    var imgURL = img.src;
+function replace_svg() {
+    document.querySelectorAll('img.svg').forEach(function(img){
+        var imgID = img.id;
+        var imgClass = img.className;
+        var imgURL = img.src;
 
-    fetch(imgURL).then(function(response) {
-        return response.text();
-    }).then(function(text){
+        fetch(imgURL).then(function(response) {
+            return response.text();
+        }).then(function(text){
 
-        var parser = new DOMParser();
-        var xmlDoc = parser.parseFromString(text, "text/xml");
+            var parser = new DOMParser();
+            var xmlDoc = parser.parseFromString(text, "text/xml");
 
-        // Get the SVG tag, ignore the rest
-        var svg = xmlDoc.getElementsByTagName('svg')[0];
+            // Get the SVG tag, ignore the rest
+            var svg = xmlDoc.getElementsByTagName('svg')[0];
 
-        // Add replaced image's ID to the new SVG
-        if(typeof imgID !== 'undefined') {
-            svg.setAttribute('id', imgID);
-        }
-        // Add replaced image's classes to the new SVG
-        if(typeof imgClass !== 'undefined') {
-            svg.setAttribute('class', imgClass+' replaced-svg');
-        }
+            // Add replaced image's ID to the new SVG
+            if(typeof imgID !== 'undefined') {
+                svg.setAttribute('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if(typeof imgClass !== 'undefined') {
+                svg.setAttribute('class', imgClass+' replaced-svg');
+            }
 
-        // Remove any invalid XML tags as per http://validator.w3.org
-        svg.removeAttribute('xmlns:a');
+            // Remove any invalid XML tags as per http://validator.w3.org
+            svg.removeAttribute('xmlns:a');
 
-        // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
-        if(!svg.getAttribute('viewBox') && svg.getAttribute('height') && svg.getAttribute('width')) {
-            svg.setAttribute('viewBox', '0 0 ' + svg.getAttribute('height') + ' ' + svg.getAttribute('width'))
-        }
+            // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+            if(!svg.getAttribute('viewBox') && svg.getAttribute('height') && svg.getAttribute('width')) {
+                svg.setAttribute('viewBox', '0 0 ' + svg.getAttribute('height') + ' ' + svg.getAttribute('width'))
+            }
 
-        // Replace image with new SVG
-        img.parentNode.replaceChild(svg, img);
+            // Replace image with new SVG
+            img.parentNode.replaceChild(svg, img);
+        });
     });
-});
+}
+
+function loadcontent(name){
+  $.get("content/"+name+".html", function(data){
+      $("#content").replaceWith(data);
+      console.log(data)
+      document.getElementById('navbar_home').classList.remove("active");
+      document.getElementById('navbar_news').classList.remove("active");
+      document.getElementById('navbar_pubs').classList.remove("active");
+      document.getElementById('navbar_talks').classList.remove("active");
+      document.getElementById('navbar_'+name).classList.add("active");
+  });
+}
