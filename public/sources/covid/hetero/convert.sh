053874ff2cf364b51211edce9d84a9d7b561461a
Johannes <johannes.zierenberg@gmail.com>
2020-08-10 14:03:17 +0200

    slides

---
+++ public/sources/covid/hetero/convert.sh
@@ -0,0 +1,8 @@
+convert -density 300 -trim ~/research/mpids/projects/covid_hetero/figures/fig_time_evolution_expectation_value_I01.pdf -quality 100 ./fig_time_evolution_expectation_value_I01.png
+convert -density 300 -trim ~/research/mpids/projects/covid_hetero/figures/fig_probability_distribution_t14_compare_k.pdf -quality 100 ./fig_probability_distribution_t14_compare_k.png
+convert -density 300 -trim ~/research/mpids/projects/covid_hetero/figures/fig_probability_distribution_approximation.pdf -quality 100 ./fig_probability_distribution_approximation.png
+convert -density 300 -trim ~/research/mpids/projects/covid_hetero/figures/fig_probability_distribution_approximation_homogeneous.pdf -quality 100 ./fig_probability_distribution_approximation_homogeneous.png
+
+convert -density 300 -trim ~/research/mpids/projects/covid_hetero/figures/fig_probability_extreme_events_I01_single.pdf -quality 100 ./fig_probability_extreme_events_I01_single.png
+convert -density 300 -trim ~/research/mpids/projects/covid_hetero/figures/fig_probability_extreme_events_I010.pdf -quality 100 ./fig_probability_extreme_events_I010.png
+convert -density 300 -trim ~/research/mpids/projects/covid_hetero/figures/fig_probability_extreme_events_t14.pdf -quality 100 ./fig_probability_extreme_events_t14.png
